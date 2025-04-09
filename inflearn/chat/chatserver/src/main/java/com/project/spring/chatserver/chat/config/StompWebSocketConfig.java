ad40811c14513ba5d1e839c9707ab311ae0d1a5d
KAH <kahback99@gmail.com>
2025-04-09 22:33:20 +0900

    Stomp 기능 화면 구현 2

--- inflearn/chat/chatserver/src/main/java/com/project/spring/chatserver/chat/config/StompWebSocketConfig.java
+++ inflearn/chat/chatserver/src/main/java/com/project/spring/chatserver/chat/config/StompWebSocketConfig.java
@@ -1,4 +1,29 @@
 package com.project.spring.chatserver.chat.config;
 
-public class StompWebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/connect")
+                .setAllowedOrigins("http://localhost:3000")
+//                ws:// 가 아닌 http:// 엔드포인트를 사용할수있게 해주는 sockJS 라이브러리를 통한 요청을 허용하는 설정
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        /publish/1 형태로 메세지 발행 설정
+//        /publish 로 시작하는 url 패턴으로 메시지가 발행되면 @Controller 객체의 @MessageMapping 메서드로 라우팅
+        registry.setApplicationDestinationPrefixes("/publish");
+
+//        /topic/1 형태로 메시지를 수신해야함을 설정
+        registry.enableSimpleBroker("/topic");
+    }
 }
